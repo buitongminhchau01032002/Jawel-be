a820506d335a1fcce2b28e98077e6582946ef78b
ThuHuong03 <ntthuong010603@gmail.com>
2023-11-12 17:27:39 +0700

    CustomerAccountControllerTest_1

--- Jawel-be/Validators/CustomerAccount/CreateCustomerAccountDtoValidator.cs
+++ Jawel-be/Validators/CustomerAccount/CreateCustomerAccountDtoValidator.cs
@@ -8,11 +8,11 @@ namespace Jawel_be.Validators.CustomerAccount
     {
         public CreateCustomerAccountDtoValidator()
         {
-            RuleFor(x => x.Phone).NotNull().NotEmpty();
-            RuleFor(x => x.Password).NotNull().MinimumLength(6);
-            RuleFor(x => x.Name).NotNull().NotEmpty();
-            RuleFor(x => x.Gender).NotNull().In("Male", "Female");
-            RuleFor(x => x.Address).NotEmpty();
+            //RuleFor(x => x.Phone).NotNull().NotEmpty();
+            //RuleFor(x => x.Password).NotNull().MinimumLength(6);
+            //RuleFor(x => x.Name).NotNull().NotEmpty();
+            //RuleFor(x => x.Gender).NotNull().In("Male", "Female");
+            //RuleFor(x => x.Address).NotEmpty();
         }
     }
 }
